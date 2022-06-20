8e03e537028e0d323c68b9382f14d39accbee655
DSolan01 <DSolan01@IN-1P51BK3>
2022-06-20 22:53:26 +0530

    1st

---
+++ Automation/src/frameWork/HelloWorld.java
@@ -0,0 +1,13 @@
+package frameWork;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+		System.out.println("Helllooooooo");
+		System.out.println("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");
+
+	}
+
+}
