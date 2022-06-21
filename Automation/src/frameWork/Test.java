5f64be1e37ff8bb5fdad129ddac3441b3ccade36
Dharmender Solanki <dharmender.solanki@merklecxm.com>
2022-06-21 22:10:41 +0530

    adding text in Test Added from git bash

--- Automation/src/frameWork/Test.java
+++ Automation/src/frameWork/Test.java
@@ -6,6 +6,7 @@ public class Test {
 		// TODO Auto-generated method stub
 		System.out.println("Helllllllll");
 		System.out.println("Sum of no");
+		System.out.println("Added from git Bash");
 	}
 
 }
