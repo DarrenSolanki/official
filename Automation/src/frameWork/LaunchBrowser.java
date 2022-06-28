534d81025477b8c8c1c6b726a68f4a06bbdc9707
DSolan01 <DSolan01@IN-1P51BK3>
2022-06-28 22:42:11 +0530

    open tabs/windows

--- Automation/src/frameWork/LaunchBrowser.java
+++ Automation/src/frameWork/LaunchBrowser.java
@@ -1,6 +1,9 @@
 package frameWork;
 
+import java.io.PrintStream;
+
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WindowType;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 //import io.github.bonigarcia.wdm.WebDriverManager;
@@ -8,17 +11,24 @@ import io.github.bonigarcia.wdm.*;
 
 public class LaunchBrowser {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		
 		WebDriverManager.chromedriver().setup();
-//		WebDriverManager.chromedriver().driverVersion("100.0").setup();
+//		WebDriverManager.chromedriver().driverVersion("93.0.4577.63").setup();
 //		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
-
+        String windowId = driver.getWindowHandle();
         driver.get("https://selenium.dev");
+        System.out.println(driver.getCurrentUrl());
+        System.out.println(driver.getTitle());
+        System.out.println("WindowId is : "+windowId);
+        driver.switchTo().newWindow(WindowType.TAB);
+        Thread.sleep(5000);
+        driver.switchTo().window(windowId);
 
-//        driver.quit();
+        driver.quit();
+      
 
 	}
 
